2824b33a4cab230adf396e045c5c7a59d768de4f
slukas111 <sashamati@gmail.com>
2020-02-25 11:29:49 -0800

    is matched

--- nested.py
+++ nested.py
@@ -3,9 +3,18 @@
 """
 Module docstring: One line description of what your program does.
 """
-__author__ = "???"
+__author__ = "Sasha Lukas w/ help Chris"
 
 import sys
+
+def is_matched(s):
+    brac_types = {
+        "parasts": ["(*", "*)"],
+        "parens": ["(", ")"],
+        "brackets": ["[", "]"],
+        "curlies": ["{", "}"],
+        "inequalities": ["<", ">"]
+    }
 
 
 def is_nested(line):
